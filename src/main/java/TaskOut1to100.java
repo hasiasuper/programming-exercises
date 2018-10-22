98abb04baa0cea817fd6e36569bddf6c1b2c80d1
hasiasuper <hasia-super@rambler.ru>
2018-10-22 22:13:30 +0300

    Update TaskOut1to100.java

--- src/main/java/TaskOut1to100.java
+++ src/main/java/TaskOut1to100.java
@@ -2,24 +2,18 @@ public class TaskOut1to100
 {
     public static void main(String[] args)
     {
-        int[] array = new int[100];
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i+1;
-        }
-
-        for (int i = 0; i < array.length; i++)
+        for (int i = 1; i <= 100; i++)
         {
-            if(array[i]%3==0 && array[i]%5==0)
+            if(i%3==0 && i%5==0)
                 System.out.println("HelloWorld");
                 else
-            if (array[i]%3==0)
+            if (i%3==0)
                 System.out.println("Hello");
             else
-            if (array[i]%5==0)
+            if (i%5==0)
                 System.out.println("World");
             else
-            System.out.println(array[i]);
+            System.out.println(i);
         }
     }
 }
