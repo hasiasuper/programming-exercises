59fe91b18ea402ec1ed6b3bd4b4ce15294228831
hasiasuper <hasia-super@rambler.ru>
2018-10-23 00:06:37 +0300

    Update TaskOut1to100.java

--- src/main/java/TaskOut1to100.java
+++ src/main/java/TaskOut1to100.java
@@ -1,19 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class TaskOut1to100
 {
     public static void main(String[] args)
     {
-        for (int i = 1; i <= 100; i++)
-        {
-            if(i%3==0 && i%5==0)
-                System.out.println("HelloWorld");
-                else
-            if (i%3==0)
-                System.out.println("Hello");
-            else
-            if (i%5==0)
-                System.out.println("World");
-            else
-            System.out.println(i);
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 1; i < 101; i++) {
+            list.add(i);
         }
+        
+        list.stream()
+                .filter(i -> i % 3 != 0 && i % 5 != 0)
+                .forEach(System.out::println);
+
+        list.stream()
+               .filter(i -> i % 3 == 0 )
+               .map((i) -> i + " Hello").collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        list.stream()
+                .filter(i -> i % 5 == 0)
+                .map((i) -> i + " World").collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        list.stream()
+                .filter(i -> i % 3 == 0 && i % 5 == 0)
+                .map((i) -> i + "HelloWorld").collect(Collectors.toList())
+                .forEach(System.out::println);
+
     }
 }
